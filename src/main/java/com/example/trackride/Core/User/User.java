db76a84942961835df606bc17ce3973d2bd2b75f
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-06 00:58:29 +0300

    feat:create User entity

---
+++ src/main/java/com/example/trackride/Core/User/User.java
@@ -0,0 +1,18 @@
+package com.example.trackride.Core.User;
+
+
+import com.example.trackride.Core.Shared.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
