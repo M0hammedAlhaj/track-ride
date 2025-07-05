cc3429eeb42148f6c31aeb622992398a21716619
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-05 21:27:19 +0300

    build:init backend with spring boot

---
+++ src/main/java/com/example/trackride/TrackRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackRideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trackride/TrackRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
