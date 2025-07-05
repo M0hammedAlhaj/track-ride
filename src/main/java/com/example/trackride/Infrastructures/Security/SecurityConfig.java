42d621e9565e2ad3030abd2543d515adf93c949d
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-06 02:10:40 +0300

    fix:secuirty inject dao authentcation

--- src/main/java/com/example/trackride/Infrastructures/Security/SecurityConfig.java
+++ src/main/java/com/example/trackride/Infrastructures/Security/SecurityConfig.java
@@ -32,6 +32,6 @@ public class SecurityConfig {
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userAuthenticationService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
-        return null;
+        return authenticationProvider;
     }
 }
