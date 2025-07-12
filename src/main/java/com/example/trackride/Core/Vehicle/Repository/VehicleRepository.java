2664c14684c127e8400113e5416b94d4a24f5178
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-12 16:12:38 +0300

    feat:create repository for vehicale

---
+++ src/main/java/com/example/trackride/Core/Vehicle/Repository/VehicleRepository.java
@@ -0,0 +1,8 @@
+package com.example.trackride.Core.Vehicle.Repository;
+
+import com.example.trackride.Core.Shared.Base.BaseRepository;
+import com.example.trackride.Core.Vehicle.Entity.Vehicle;
+
+public interface VehicleRepository extends BaseRepository<Vehicle> {
+
+}
