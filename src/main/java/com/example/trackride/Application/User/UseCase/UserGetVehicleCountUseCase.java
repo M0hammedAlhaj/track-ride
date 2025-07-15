e2c818f492699c4c8ad80fc43cbc863c7ac4ad83
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-15 23:50:03 +0300

    fix:injection vehicleRepository

--- src/main/java/com/example/trackride/Application/User/UseCase/UserGetVehicleCountUseCase.java
+++ src/main/java/com/example/trackride/Application/User/UseCase/UserGetVehicleCountUseCase.java
@@ -1,13 +1,15 @@
 package com.example.trackride.Application.User.UseCase;
 
 import com.example.trackride.Core.Vehicle.Repository.VehicleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserGetVehicleCountUseCase {
-    private VehicleRepository vehicleRepository;
+    private final VehicleRepository vehicleRepository;
 
     public Long execute(String userId) {
         return vehicleRepository.countVehiclesByOwnerId(UUID.fromString(userId));
