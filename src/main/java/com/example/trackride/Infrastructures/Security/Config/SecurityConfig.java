7dfa639025d73018890bd58fcf68989a496bc186
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-15 23:47:49 +0300

    feat:sucre count vehicles

--- src/main/java/com/example/trackride/Infrastructures/Security/Config/SecurityConfig.java
+++ src/main/java/com/example/trackride/Infrastructures/Security/Config/SecurityConfig.java
@@ -22,14 +22,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-                .cors()
-                .and()
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users/assign-vehicle").authenticated()
                         .requestMatchers("/api/v1/users/assign-maintenance").authenticated()
                         .requestMatchers("/api/v1/users/vehicles").authenticated()
                         .requestMatchers("/api/v1/users/last-Maintenance").authenticated()
+                        .requestMatchers("/api/v1/users/count-vehicles").authenticated()
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
