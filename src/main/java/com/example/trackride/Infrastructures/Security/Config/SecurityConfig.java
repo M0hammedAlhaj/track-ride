437898cb9dd647e647bb4e5b3452521608e81b88
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-17 15:06:15 +0300

    chore:change port of frontend

--- src/main/java/com/example/trackride/Infrastructures/Security/Config/SecurityConfig.java
+++ src/main/java/com/example/trackride/Infrastructures/Security/Config/SecurityConfig.java
@@ -19,9 +19,12 @@ public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
+    private final CorsConfig corsConfig
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users/assign-vehicle").authenticated()
