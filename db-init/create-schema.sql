5942c691788d890863b62848532fcfaca1fa0603
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-07-21 21:51:43 +0300

    chore:fix database connection in schema trakride

--- db-init/create-schema.sql
+++ db-init/create-schema.sql
@@ -1,1 +1,5 @@
+-- Connect to the trackride database and create schema
+\c trackride;
 CREATE SCHEMA IF NOT EXISTS trackride;
+-- Set the search path to include the trackride schema
+ALTER DATABASE trackride SET search_path TO trackride, public;
